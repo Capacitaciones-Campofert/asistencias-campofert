061a67eb51b8002eb724eb86501f8f24536bebb9
Capacitaciones-Campofert <gestionhumana@campofert.com>
2026-04-23 13:54:40 -0500

    Update app.py

--- app.py
+++ app.py
@@ -292,6 +292,26 @@ if 'rol' not in st.session_state:
         st.markdown('<div class="footer-premium">Campofert S.A.S • Campolab • Versión Ejecutiva 2026</div>', unsafe_allow_html=True)
 
     st.stop()
+
+# =============================================================================
+# INTERFAZ
+# =============================================================================
+
+col_logo1, col_logo2, col_logo3 = st.columns([1, 1, 1])
+with col_logo1:
+    if os.path.exists("logo_campofert.png"):
+        img = Image.open("logo_campofert.png")
+        img.thumbnail((260, 120), Image.LANCZOS)
+        st.image(img)
+with col_logo3:
+    if os.path.exists("logo_campolab.png"):
+        img2 = Image.open("logo_campolab.png")
+        img2.thumbnail((260, 120), Image.LANCZOS)
+        st.image(img2)
+        
+st.markdown("<h1 style='text-align:center; color:#1B5E20;'>Registro de Capacitación</h1>", unsafe_allow_html=True)
+st.markdown("---")
+
 # =============================================================================
 # MENÚ SEGÚN ROL + PANEL ADMIN V3.0 FINAL (SIN CONFLICTOS)
 # Reemplaza TODO tu bloque actual desde "MENÚ SEGÚN ROL"
@@ -733,24 +753,6 @@ def generar_pdf(datos, imagen_firma, imagen_foto):
     buffer.seek(0)
 
     return buffer
-# =============================================================================
-# INTERFAZ
-# =============================================================================
-
-col_logo1, col_logo2, col_logo3 = st.columns([1, 1, 1])
-with col_logo1:
-    if os.path.exists("logo_campofert.png"):
-        img = Image.open("logo_campofert.png")
-        img.thumbnail((260, 120), Image.LANCZOS)
-        st.image(img)
-with col_logo3:
-    if os.path.exists("logo_campolab.png"):
-        img2 = Image.open("logo_campolab.png")
-        img2.thumbnail((260, 120), Image.LANCZOS)
-        st.image(img2)
-        
-st.markdown("<h1 style='text-align:center; color:#1B5E20;'>Registro de Capacitación</h1>", unsafe_allow_html=True)
-st.markdown("---")
 
 # =============================================================================
 # OPCIÓN 1: REGISTRO DE ASISTENCIA
