13a7eff07344634da0aadf7fa05b0cfa09a9959d
Capacitaciones-Campofert <gestionhumana@campofert.com>
2026-05-07 16:26:11 -0500

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ from email import encoders
 # CONFIGURACIÓN DE PÁGINA
 # =============================================================================
 st.set_page_config(
-    page_title="REGISTRO DE ASISTENCIA DIGILTAL",
+    page_title="REGISTRO DE ASISTENCIA DIGITAL",
     layout="centered",
     page_icon=""
 )
