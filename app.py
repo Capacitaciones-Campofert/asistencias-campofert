16efb4385b031ebcfe29989f4e6345a73ae73aa4
Capacitaciones-Campofert <gestionhumana@campofert.com>
2026-05-13 11:20:27 -0500

    Update app.py

--- app.py
+++ app.py
@@ -762,8 +762,8 @@ if st.session_state.get("rol") is None:
         </div>
         <h1>REGISTRO ASISTENCIA DIGITAL</h1>
         <div class="hero-mini">
-            Código: I.FO.GH.03 | Versión: 03 | Fecha de emisión: 2014-12-01 |
-            Fecha de actualización: 2026-05-20{texto_pagina}
+            Código: I.FO.GH.65 | Versión: 01 | Fecha de emisión: 2026-05-13 |
+            Fecha de actualización: 2026-05-13{texto_pagina}
         </div>
     </div>
     """, unsafe_allow_html=True)
